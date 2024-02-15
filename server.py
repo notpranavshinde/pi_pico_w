8e565d8bdc5254640961a4cdeee036349d1a041c
notpranavshinde <138583678+notpranavshinde@users.noreply.github.com>
2024-02-14 17:19:58 -0700

    Update server.py
    refactored

--- server.py
+++ server.py
@@ -1,46 +1,61 @@
-from machine import Pin
+from machine import Pin, ADC
 import network
 import socket
 import time
-from secret import ssid, password
-# to be added: another file with network configs
+import secret
 
-# Connect to network
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-wlan.connect(ssid, password)
+def connect_to_wifi(ssid, password, max_wait=20):
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid, password)
+    
+    while max_wait > 0:
+        if wlan.status() < 0 or wlan.status() >= 3:
+            break
+        max_wait -= 1
+        print('waiting for connection...')
+        time.sleep(1)
+    
+    if wlan.status() != 3:
+        raise RuntimeError('network connection failed')
+    else:
+        print('connected')
+        status = wlan.ifconfig()
+        print('ip = ' + status[0])
+    return wlan
 
-while not wlan.isconnected() and wlan.status() >= 0:
-    print("Waiting to connect:")
-    time.sleep(1)
-
-print(wlan.ifconfig())
-
-# Set up server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('', 80))
-s.listen(5)
-
-while True:
-    conn, addr = s.accept()
-    print('Got a connection from %s' % str(addr))
-    request = conn.recv(1024)
-    print('Content = %s' % str(request))
-
-    # Generate string
-    response = "Hello, Pico W!"
-
-    # Send string
-    conn.send(response)
-    conn.close()
-
-    # Receive string
-    ai = socket.getaddrinfo("192.168.0.23", 80)#replace this ip with the ip of the other pico.
-    addr = ai[-1]
+def create_socket():
+    addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+    
     s = socket.socket()
-    s.connect(addr)
-    s.send(b"GET Data")
-    received_string = str(s.recv(512))
-    print(received_string)
-    s.close()
-    time.sleep(0.2)
+    s.bind(addr)
+    s.listen(1)
+    print('listening on', addr)
+    return s
+
+def handle_client(s):
+    while True:
+        try:
+            cl, addr = s.accept()
+            print("Connection from:", addr)
+            request = cl.recv(1024).decode()
+            print("Received:", request)
+            
+            response = "something to send"
+            cl.send(response.encode())
+            print("Sent:", response)
+            
+            cl.close()
+        except OSError as e:
+            cl.close()
+            print('connection closed')
+
+def main():
+    ssid = secret.ssid
+    password = secret.password
+    connect_to_wifi(ssid, password)
+    s = create_socket()
+    handle_client(s)
+
+if __name__ == "__main__":
+    main()
